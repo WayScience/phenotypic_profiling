8f8502bdb74e325a098d11e0a6aaefc8e15eda44
roshankern <roshan.kern@ucdenver.edu>
2022-07-25 10:53:58 -0600

    intelligent holdout data

--- 2.ML_model/utils/MlPipelineUtils.py
+++ 2.ML_model/utils/MlPipelineUtils.py
@@ -34,28 +34,102 @@ def get_features_data(load_path: pathlib.Path) -> pd.DataFrame:
 
     return features_data
 
-
-def get_random_images_indexes(training_data: pd.DataFrame, num_images: int) -> List:
-    """get image indexes from training dataset
-
-    Args:
-        training_data (pd.DataFrame): pandas dataframe of training data
-        num_images (int): number of images to holdout
-
-    Returns:
-        List: list of image indexes to with held out images
-    """
-    unique_images = pd.unique(training_data["Metadata_Plate_Map_Name"])
-    images = np.random.choice(unique_images, size=num_images, replace=False)
-
+def get_image_indexes(training_data: pd.DataFrame, images: List) -> List:
+    
     image_indexes_list = []
     for image in images:
         image_indexes = training_data.index[
             training_data["Metadata_Plate_Map_Name"] == image
         ].tolist()
         image_indexes_list.extend(image_indexes)
-
+        
     return image_indexes_list
+
+def get_random_images_indexes(training_data: pd.DataFrame, num_images: int) -> List:
+    """get ramdom images from training dataset
+
+    Args:
+        training_data (pd.DataFrame): pandas dataframe of training data
+        num_images (int): number of images to holdout
+
+    Returns:
+        List: list of unique images for holding out
+    """
+    unique_images = pd.unique(training_data["Metadata_Plate_Map_Name"])
+    images = np.random.choice(unique_images, size=num_images, replace=False)
+    
+    return images
+    
+
+def get_intelligent_images(training_data: pd.DataFrame, num_images: int) -> List:
+    """get images from training dataset and try to balance labels present in these images
+    add an image if it has at least class not represented by the other images
+    if the image doesn't have a contribution via new class, try a new image
+
+    Args:
+        training_data (pd.DataFrame): pandas dataframe of training data
+        num_images (int): number of images to holdout
+
+    Returns:
+        List: list of unique images with intelligently balanced phenotypic classes
+    """
+    remaining_images = pd.unique(training_data["Metadata_Plate_Map_Name"])
+    remaining_classes = pd.unique(training_data["Mitocheck_Phenotypic_Class"]).tolist()
+    
+    images = []
+    
+    for image_number in range(num_images):
+        image_contributes = False
+        image = ""
+        while not image_contributes:
+            image = np.random.choice(remaining_images, size=1, replace=False)[0]
+            image_data = training_data.loc[(training_data['Metadata_Plate_Map_Name'] == image)]
+            
+            if len(remaining_classes) == 0:
+                image_contributes = True
+            
+            image_classes = pd.unique(image_data["Mitocheck_Phenotypic_Class"])
+            # check if any phenotypic class from the current image is in the remaining classes
+            if any(phenotypic_class in image_classes for phenotypic_class in remaining_classes):
+                image_contributes = True
+                remaining_classes = [x for x in remaining_classes if x not in image_classes]
+        
+        images.append(image)
+        remaining_images = np.delete(remaining_images, np.where(remaining_images == image))
+        
+    return images
+
+def get_representative_images(training_data: pd.DataFrame, num_images: int, attempts: int=100) -> List:
+    """get images from training dataset and such that every phenotypic class is represented
+    returns None if no combintation of images are found that represent every phenotypic class within number of trials
+
+    Args:
+        training_data (pd.DataFrame): pandas dataframe of training data
+        num_images (int): number of images to holdout
+        attempts (int): number of times to try getting representative images
+
+    Returns:
+        List: list of images with every phenotypic class represented or None if this list cannot be curated
+    """
+    unique_classes = pd.unique(training_data["Mitocheck_Phenotypic_Class"]).tolist()
+    
+    trial = 0
+    while trial < attempts:
+        images = get_intelligent_images(training_data, num_images)
+        
+        images_data = pd.DataFrame()
+        for image in images:
+            image_data = training_data.loc[(training_data['Metadata_Plate_Map_Name'] == image)]
+            images_data = pd.concat([images_data, image_data])
+        
+        unique_image_classes = pd.unique(images_data["Mitocheck_Phenotypic_Class"]).tolist()
+        if set(unique_image_classes) == set(unique_classes):
+            return images
+        
+        trial += 1
+    
+    print("No combination of images found that represents all classes!")
+    return None
 
 
 def get_dataset(
