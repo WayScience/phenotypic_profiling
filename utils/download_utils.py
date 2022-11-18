995790779adb62152c5816fab71b3281182c11cf
roshankern <roshan.kern@ucdenver.edu>
2022-11-18 12:17:57 -0700

    combine datasets

---
+++ utils/download_utils.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
+def parse_outline_data(raw_outline_data: str) -> np.array:
+    """
+    parse outline data extracted with IDR stream into numpy array format
+    Parameters
+    ----------
+    raw_outline_data : str
+        string of outline data
+    Returns
+    -------
+    np.array
+        parsed outline data
+    """
+    outline_data = []
+
+    raw_outline_data = raw_outline_data[1:-1]
+    raw_outline_data = raw_outline_data.split("\n ")
+    for coord_string in raw_outline_data:
+        x = int(coord_string[1:-1].split()[0])
+        y = int(coord_string[1:-1].split()[1])
+        outline_data.append([x, y])
+
+    return np.array(outline_data)
+
+def combine_datasets(training_data_2006: pd.DataFrame, training_data_2015: pd.DataFrame) -> pd.DataFrame:
+    """
+    combine 2006 and 2015 datasets without cell repeats
+
+    Parameters
+    ----------
+    training_data_2006 : pd.DataFrame
+        training dataset from 2006
+    training_data_2015 : pd.DataFrame
+        training dataset from 2015, includes object outline data
+
+    Returns
+    -------
+    pd.DataFrame
+        dataframe with single cell data from 2006 and 2015 without duplicates
+    """
+    non_duplicate_2015 = []
+    
+    for index_2015, row_2015 in training_data_2015.iterrows():
+        in_both = False
+        for _, row_2006 in training_data_2006.iterrows():
+            # check if cell from 2015 data is in same plate, well, frame as 2006 data and in the same location (within cell outline)
+            if row_2015["Metadata_Plate"] == row_2006["Metadata_Plate"] and row_2015["Metadata_Well"] == row_2006["Metadata_Well"] and row_2015["Metadata_Frame"] == row_2006["Metadata_Frame"]:
+                outline_data_2015 = parse_outline_data(row_2015["Object_Outline"])
+                cell_polygon_2015 = Polygon(outline_data_2015)
+                point_2006 = Point(int(row_2006["Location_Center_X"]), int(row_2006["Location_Center_X"]))
+                if cell_polygon_2015.contains(point_2006):
+                    print(f"Cell found in both datasets! Index {index_2015} in 2015 data.")
+                    in_both = True
+        
+        if not in_both:
+            non_duplicate_2015.append(row_2015.drop(labels=["Object_Outline"]))
+            
+    non_duplicate_2015 =  pd.DataFrame(non_duplicate_2015)
+    return pd.concat([training_data_2006, non_duplicate_2015], ignore_index=True)
