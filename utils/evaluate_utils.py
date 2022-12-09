cbc3ef87f08402bc46cbf5f45df1206aa8cf307e
roshankern <roshan.kern@ucdenver.edu>
2022-12-09 15:57:44 -0700

    add PR curves

--- utils/evaluate_utils.py
+++ utils/evaluate_utils.py
@@ -13,8 +13,73 @@ import seaborn as sns
 
 from train_utils import get_X_y_data
 
+from sklearn.metrics import precision_recall_curve
+from sklearn.preprocessing import label_binarize
+
 # set numpy seed to make random operations reproduceable
 np.random.seed(0)
+
+def class_PR_curves(single_cell_data: pd.DataFrame, log_reg_model):
+    """
+    save precision recall curves for each class to the save directory
+    also, return the precision/recall data for each class in tidy long format
+
+    Parameters
+    ----------
+    single_cell_data : pd.DataFrame
+        single cell dataframe with correct cell metadata and feature data
+    log_reg_model : sklearn classifier
+        clasifier to get precision recall curves for
+
+    Returns
+    -------
+    pd.DataFrame
+        dataframe with precision/recall data for each class in tidy long format
+        
+    matplotlib.figure.Figure
+        figure with compilation of all class PR curves
+    """
+    
+    phenotypic_classes = log_reg_model.classes_
+    X, y = get_X_y_data(single_cell_data)
+    
+    # binarize labels for precision recall curve function    
+    y_binarized = label_binarize(y, classes=phenotypic_classes)
+    # predict class probabilities for feature data
+    y_score = log_reg_model.predict_proba(X)
+    
+    # data from PR curves will be stored in tidy data format (eventually pandas dataframe)
+    PR_data = []
+    # which thresholds the precision/recalls correspond to
+    PR_threshold = np.arange(single_cell_data.shape[0])
+    # last values in precision/recall curve don't correspond to cell dataset
+    PR_threshold = np.append(PR_threshold, None)
+    
+    fig, axs = plt.subplots(4,4)
+    fig.set_size_inches(15, 15)
+    ax_x = 0
+    ax_y = 0
+    for i in range(phenotypic_classes.shape[0]):
+        precision, recall, _ = precision_recall_curve(y_binarized[:, i], y_score[:, i])
+        class_tidy_data = pd.DataFrame({"Phenotypic_Class": phenotypic_classes[i],"PR_Threshold": PR_threshold, "Precision": precision, "Recall": recall})
+        PR_data.append(class_tidy_data)
+        
+        axs[ax_x, ax_y].plot(recall, precision, lw=2, label=phenotypic_classes[i])
+        axs[ax_x, ax_y].set_title(phenotypic_classes[i])
+        axs[ax_x, ax_y].set(xlabel="Recall", ylabel="Precision")
+        
+        ax_x += 1
+        if ax_x == 4:
+            ax_x = 0
+            ax_y += 1
+    
+    # only label outer plots
+    for ax in axs.flat:
+        ax.label_outer()
+
+    PR_data = pd.concat(PR_data, axis=0).reset_index(drop=True)
+    return fig, PR_data
+
 
 def evaluate_model_cm(
     log_reg_model: LogisticRegression, dataset: pd.DataFrame
