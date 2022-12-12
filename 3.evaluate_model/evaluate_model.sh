2240a1b9de8932067e60e172894885dfe131f35d
roshankern <roshan.kern@ucdenver.edu>
2022-12-12 15:23:20 -0700

    greg recommendations

--- 3.evaluate_model/evaluate_model.sh
+++ 3.evaluate_model/evaluate_model.sh
@@ -2,4 +2,8 @@
 # Convert notebooks to python file and execute
 jupyter nbconvert --to python \
         --FilesWriter.build_directory=scripts/nbconverted \
-        --execute *.ipynb
+        --execute evaluate_model.ipynb
+
+jupyter nbconvert --to python \
+        --FilesWriter.build_directory=scripts/nbconverted \
+        --execute class_PR_curves.ipynb
