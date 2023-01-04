bcf68728d5408918238986f31e0b6dae1fa70f59
Roshan Kern <105665675+roshankern@users.noreply.github.com>
2023-01-04 15:53:41 -0700

    Create validation module (#7)
    * Use 2015 data & remove holdout set (#5)
    
    * finish download module changes
    
    * download notebook
    
    * rerun split data module
    
    * rerun download module
    
    * rerun train_model
    
    * rerun evaluation module
    
    * rerun interpretation module
    
    * combine datasets
    
    * combine datasets
    
    * split changes
    
    * update format
    
    * format update
    
    * format
    
    * finish split data
    
    * combine datasets, remove holdout
    
    * formatting
    
    * rerun pipelines
    
    * remove folded class
    
    * rerun pipeline
    
    * Update utils/download_utils.py
    
    Co-authored-by: Dave Bunten <ekgto445@gmail.com>
    
    * PR fixes
    
    * module docstrings
    
    Co-authored-by: Dave Bunten <ekgto445@gmail.com>
    
    * validation module
    
    * correlation matrix
    
    * reviz
    
    * reformat
    
    * pearson correlation
    
    * spearman correlation
    
    * documentation
    
    * documentation
    
    * docs
    
    * docs, rerun
    
    * docs
    
    * docs
    
    * Update 5.validate_model/validate_model.sh
    
    Co-authored-by: Dave Bunten <ekgto445@gmail.com>
    
    * Update utils/validate_utils.py
    
    Co-authored-by: Dave Bunten <ekgto445@gmail.com>
    
    * raw link clarification
    
    * Update utils/validate_utils.py
    
    Co-authored-by: Dave Bunten <ekgto445@gmail.com>
    
    * Update utils/validate_utils.py
    
    Co-authored-by: Dave Bunten <ekgto445@gmail.com>
    
    * Update utils/validate_utils.py
    
    Co-authored-by: Dave Bunten <ekgto445@gmail.com>
    
    * conditional to remove x, y columns
    
    * clarify perturbation rename
    
    * black formatting
    
    Co-authored-by: Dave Bunten <ekgto445@gmail.com>

---
+++ 5.validate_model/scripts/nbconverted/cell_health_validation.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Import Libraries
+
+# In[1]:
+
+
+import pandas as pd
+import pathlib
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import sys
+sys.path.append("../utils")
+from validate_utils import create_classification_profiles
+
+
+# ### Load Cell Health Profile Labels
+
+# In[2]:
+
+
+cell_health_hash = "30ea5de393eb9cfc10b575582aa9f0f857b44c59"
+cell_health_labels_link = f"https://raw.github.com/broadinstitute/cell-health/{cell_health_hash}/1.generate-profiles/data/consensus/cell_health_median.tsv.gz"
+
+cell_health_labels = pd.read_csv(cell_health_labels_link, compression="gzip", sep="\t")
+cell_health_labels
+
+
+# ### Create Classification Profiles
+
+# In[3]:
+
+
+cell_health_data_hash = "4ee08b16f4b5c5266309a295b0a1697f0b9540b8"
+plate_classifications_dir_link = f"https://github.com/WayScience/cell-health-data/raw/{cell_health_data_hash}/4.classify-features/plate_classifications"
+cell_line_plates = {
+    "A549": ["SQ00014610", "SQ00014611", "SQ00014612"],
+    "ES2": ["SQ00014613", "SQ00014614", "SQ00014615"],
+    "HCC44": ["SQ00014616", "SQ00014617", "SQ00014618"],
+}
+
+classification_profiles = create_classification_profiles(plate_classifications_dir_link, cell_line_plates)
+classification_profiles
+
+
+# In[4]:
+
+
+# combine cell health label profiles and classification profiles on perturbation and cell line
+final_profile_dataframe = pd.merge(cell_health_labels, classification_profiles, on=["Metadata_pert_name", "Metadata_cell_line"])
+final_profile_dataframe
+
+
+# ### Find Correlation
+
+# In[5]:
+
+
+correlation_method = "pearson"
+
+# combine cell health label profiles and classification profiles on perturbation and cell line
+final_profile_dataframe = pd.merge(cell_health_labels, classification_profiles, on=["Metadata_pert_name", "Metadata_cell_line"])
+# find correlation
+corr = final_profile_dataframe.corr(method=correlation_method)
+# convert correlation to diagram-friendly format
+corr_graph = corr.iloc[70:, :70]
+corr_graph
+
+
+# ### Show Correlation with Clustermap
+
+# In[6]:
+
+
+sns.clustermap(corr_graph, 
+            xticklabels=corr_graph.columns,
+            yticklabels=corr_graph.index,
+            cmap='RdBu_r',
+            linewidth=0.5,
+            figsize=(20,10))
+
+
+# ### Save Correlation Data in Tidy Long Format
+
+# In[7]:
+
+
+corr_data_save_path = pathlib.Path(f"validations/cell_health_{correlation_method}_correlations.tsv")
+corr_data_save_path.parents[0].mkdir(parents=True, exist_ok=True)
+
+tidy_data = corr_graph.stack()
+tidy_data = pd.DataFrame(tidy_data).reset_index(level=[0,1])
+tidy_data.columns = ["Phenotypic_Class", "Cell_Health_Label", "Correlation"]
+
+tidy_data.to_csv(corr_data_save_path, sep="\t")
+tidy_data
+
---
+++ utils/validate_utils.py
@@ -0,0 +1,83 @@
+"""
+utilities for validating logistic regression models on MitoCheck single-cell dataset
+"""
+
+import pandas as pd
+
+
+def create_classification_profiles(
+    plate_classifications_dir_link: str, cell_line_plates: dict
+) -> pd.DataFrame:
+    """
+    create classsification profiles for correlation to cell health label profiles
+    a classification profile consists of classification probabilities averaged across perturbation and cell line
+
+    Parameters
+    ----------
+    plate_classifications_dir_link : str
+        raw GitHub link to plate classifications directory
+    cell_line_plates : dict
+        cell line names, each with a list of plate names that are correlated to their respective cell line
+
+    Returns
+    -------
+    pd.DataFrame
+        classification profiles dataframe
+    """
+
+    cell_line_classification_profiles = []
+
+    for cell_line in cell_line_plates:
+        # create one large dataframe for all 3 plates in the particular cell line
+        cell_line_plate_names = cell_line_plates[cell_line]
+        cell_line_plate_classifications = []
+        for cell_line_plate_name in cell_line_plate_names:
+            plate_classifications_link = f"{plate_classifications_dir_link}/{cell_line_plate_name}_cell_classifications.csv.gz"
+            plate_classifications = pd.read_csv(
+                plate_classifications_link, compression="gzip", index_col=0
+            )
+            cell_line_plate_classifications.append(plate_classifications)
+        cell_line_plate_classifications = pd.concat(
+            cell_line_plate_classifications, axis=0
+        ).reset_index(drop=True)
+
+        # create dataframe with cell classifications averaged across perturbation, include cell line metadata
+        # add cell line metadata
+        cell_line_plate_classifications["Metadata_cell_line"] = cell_line
+        # rename perturbation column to match the format of cell health label profiles, in this case "perturbation" corresponds to "reagent" because DeepProfiler (used much earlier in pipeline) makes no distinction
+        cell_line_plate_classifications = cell_line_plate_classifications.rename(
+            columns={"Metadata_Reagent": "Metadata_pert_name"}
+        )
+
+        # get rid of extra metadata columns
+        phenotypic_classes = [
+            col
+            for col in cell_line_plate_classifications.columns.tolist()
+            if ("Metadata" not in col)
+            or (col not in ["Location_Center_X", "Location_Center_Y"])
+        ]
+        columns_to_keep = [
+            "Metadata_pert_name",
+            "Metadata_cell_line",
+        ] + phenotypic_classes
+        cell_line_plate_classifications = cell_line_plate_classifications[
+            columns_to_keep
+        ]
+
+        # average across perturbation
+        cell_line_classification_profile = cell_line_plate_classifications.groupby(
+            ["Metadata_pert_name", "Metadata_cell_line"]
+        ).mean()
+        cell_line_classification_profiles.append(cell_line_classification_profile)
+
+    classification_profiles = pd.concat(cell_line_classification_profiles, axis=0)
+    # convert pandas index to columns
+    classification_profiles = classification_profiles.reset_index(
+        level=["Metadata_pert_name", "Metadata_cell_line"]
+    )
+    # convert no reagent to empty in pert column to follow format of cell health label profiles
+    classification_profiles = classification_profiles.replace(
+        {"Metadata_pert_name": "no reagent"}, "EMPTY"
+    )
+
+    return classification_profiles
