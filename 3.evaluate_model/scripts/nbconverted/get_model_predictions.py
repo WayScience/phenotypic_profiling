f8d1c2409b771fc0bf10f0dddcbc65980682b6b8
roshankern <roshan.kern@ucdenver.edu>
2023-02-14 13:36:11 -0700

    dave suggestions

--- 3.evaluate_model/scripts/nbconverted/get_model_predictions.py
+++ 3.evaluate_model/scripts/nbconverted/get_model_predictions.py
@@ -45,34 +45,41 @@ compiled_predictions = []
 # iterate through each model (final model, shuffled baseline model, etc)
 for model_path in models_dir.iterdir():
     model = load(model_path)
-    model_name = model_path.name.replace("log_reg_","").replace(".joblib","")
-    
+    model_name = model_path.name.replace("log_reg_", "").replace(".joblib", "")
+
     # iterate through label datasets (labels correspond to train, test, etc)
     # with nested for loops, we test each model on each dataset(corresponding to a label)
     for label in data_split_indexes["label"].unique():
         print(f"Getting predictions for {model_name} on dataset {label}")
-        
+
         # get indexes of each cell being predicted for the dataset the cell is from
-        dataset_indexes = data_split_indexes.loc[data_split_indexes["label"] == label]["index"]
-        
+        dataset_indexes = data_split_indexes.loc[data_split_indexes["label"] == label][
+            "index"
+        ]
+
         # load dataset (train, test, etc)
         data = get_dataset(features_dataframe, data_split_indexes, label)
-        
+
         # get features and labels dataframes
         X, y = get_X_y_data(data)
 
         # get predictions from model
         y_pred = model.predict(X)
-        
-        # create dataframe with dataset index of cell being predicted, predicted phenotypic class, true phenotypic class, and which dataset/model is involved in prediction
+
+        # create dataframe with dataset index of cell being predicted, 
+        # predicted phenotypic class, 
+        # true phenotypic class, 
+        # and which dataset/models are involved in prediction
         predictions_df = pd.DataFrame(
-        {'Dataset_Index': dataset_indexes,
-        'Phenotypic_Class_Predicted': y,
-        'Phenotypic_Class_True': y_pred,
-        'data_split': label,
-        "shuffled": "shuffled" in model_name
-        })
-        
+            {
+                "Dataset_Index": dataset_indexes,
+                "Phenotypic_Class_Predicted": y,
+                "Phenotypic_Class_True": y_pred,
+                "data_split": label,
+                "shuffled": "shuffled" in model_name,
+            }
+        )
+
         compiled_predictions.append(predictions_df)
 
 
