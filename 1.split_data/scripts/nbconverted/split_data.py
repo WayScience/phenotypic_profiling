37f93a2bf3ecc42585ad307ec784a52a911cc011
roshankern <roshan.kern@ucdenver.edu>
2023-03-09 16:39:15 -0700

    refactor module

--- 1.split_data/scripts/nbconverted/split_data.py
+++ 1.split_data/scripts/nbconverted/split_data.py
@@ -8,10 +8,9 @@
 # In[1]:
 
 
+import pathlib
 import pandas as pd
 import numpy as np
-import pathlib
-
 from sklearn.utils import shuffle
 
 import sys
@@ -25,27 +24,29 @@ from split_utils import get_features_data
 
 
 # load x (features) and y (labels) dataframes
-load_path = pathlib.Path("../0.download_data/data/training_data.csv.gz")
-training_data = get_features_data(load_path)
-print(training_data.shape)
-
-# ratio of data to be reserved for testing (ex 0.15 = 15%)
-test_ratio = 0.15
+labeled_data_path = pathlib.Path("../0.download_data/data/labeled_data.csv.gz")
+labeled_data = get_features_data(labeled_data_path)
+print(labeled_data.shape)
 
 
 # In[3]:
 
 
+# ratio of data to be reserved for testing (ex 0.15 = 15%)
+test_ratio = 0.15
+
 # test_data is pandas dataframe with test split, stratified by Mitocheck_Phenotypic_Class
-test_data = training_data.groupby("Mitocheck_Phenotypic_Class", group_keys=False).apply(
+testing_data = labeled_data.groupby("Mitocheck_Phenotypic_Class", group_keys=False).apply(
     lambda x: x.sample(frac=test_ratio)
 )
-test_indexes = test_data.index
-# remove test indexes
-training_data = training_data.drop(pd.Index(data=test_indexes))
+test_indexes = testing_data.index
 
+# training data is labeled data - test data
+training_data = labeled_data.drop(pd.Index(data=test_indexes))
 train_indexes = np.array(training_data.index)
-print(training_data.shape)
+
+print(f"Training data has shape: {training_data.shape}")
+print(f"Testing data has shape: {testing_data.shape}")
 
 
 # In[4]:
@@ -54,10 +55,13 @@ print(training_data.shape)
 # create pandas dataframe with all indexes and their respective labels
 index_data = []
 for index in test_indexes:
-    index_data.append({"label": "test", "index": index})
+    index_data.append({"labeled_data_index": index, "label": "test"})
 for index in train_indexes:
-    index_data.append({"label": "train", "index": index})
+    index_data.append({"labeled_data_index": index, "label": "train"})
 index_data = pd.DataFrame(index_data)
+# put indexes into sorted order
+index_data = index_data.sort_values(["labeled_data_index"])
+
 index_data
 
 
