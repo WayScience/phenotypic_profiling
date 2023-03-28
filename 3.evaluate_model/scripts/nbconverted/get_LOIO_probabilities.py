076cc4187f081eba72fec0d11b51ecbca64a41f7
roshankern <roshan.kern@ucdenver.edu>
2023-03-28 17:49:03 -0600

    rerun notebook

--- 3.evaluate_model/scripts/nbconverted/get_LOIO_probabilities.py
+++ 3.evaluate_model/scripts/nbconverted/get_LOIO_probabilities.py
@@ -8,16 +8,11 @@ import pathlib
 import warnings
 
 import pandas as pd
-import numpy as np
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import (
-    StratifiedKFold,
-    GridSearchCV,
-)
-from sklearn.utils import shuffle, parallel_backend
+from sklearn.utils import parallel_backend
 from sklearn.exceptions import ConvergenceWarning
-from joblib import dump, load
+from joblib import load
 
 import sys
 
@@ -41,7 +36,7 @@ labeled_data.head(5)
 # In[3]:
 
 
-# see number of images to 
+# see number of images to
 num_images = labeled_data["Metadata_DNA"].unique().shape[0]
 print(f"There are {num_images} images to perform LOIO evaluation on per model.")
 
@@ -60,11 +55,11 @@ count = 0
 # iterate through each model (final model, shuffled baseline model, etc)
 # sorted so final models are loaded before shuffled_baseline
 for model_path in sorted(models_dir.iterdir()):
-    
     # only perform LOIO with hyper params from final models
     if "shuffled" in model_path.name:
         continue
 
+    # load the model
     model = load(model_path)
     # determine feature type from model file name
     feature_type = model_path.name.split("__")[1].replace(".joblib", "")
@@ -122,18 +117,18 @@ for model_path in sorted(models_dir.iterdir()):
 
         # combine metadata and probabilities dataframes for test cells to create wide data
         test_cells_wide_data = pd.concat([metadata_dataframe, probas_dataframe], axis=1)
-        
+
         # add tidy long data to compiled data
         compiled_LOIO_wide_data.append(test_cells_wide_data)
-        
+
         # DELETE THE REST OF THE LINES IN THIS NOTEBOOK BEFORE FINAL RUN
         score = LOIO_model.score(X_test, y_test)
         print(
             f"Leaving out image: {image_path}; number of cells: {test_cells.shape[0]}, score: {score}"
         )
 
-        count +=1
-        if count%5==0:
+        count += 1
+        if count % 5 == 0:
             break
 
 
@@ -161,10 +156,13 @@ LOIO_probas_dir = pathlib.Path("evaluations/LOIO_probas/")
 LOIO_probas_dir.mkdir(parents=True, exist_ok=True)
 
 # define save path
-compiled_LOIO_save_path = pathlib.Path(f"{LOIO_probas_dir}/compiled_LOIO_probabilites.tsv")
+compiled_LOIO_save_path = pathlib.Path(
+    f"{LOIO_probas_dir}/compiled_LOIO_probabilites.tsv"
+)
 
 # save data as tsv
 compiled_LOIO_tidy_long_data.to_csv(compiled_LOIO_save_path, sep="\t")
 
+# preview tidy long data
 compiled_LOIO_tidy_long_data
 
