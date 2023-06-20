e5cd9db148cd4c6287ca2e8f4c103852dfc2afa9
roshankern <roshan.kern@ucdenver.edu>
2023-06-19 19:07:57 -0600

    formatting, documentation

--- 5.validate_model/scripts/nbconverted/cell_health_correlations.py
+++ 5.validate_model/scripts/nbconverted/cell_health_correlations.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 # ### Import Libraries
-# 
+#
 
 # In[1]:
 
@@ -43,7 +43,7 @@ tidy_long_corrs_save_dir.mkdir(exist_ok=True, parents=True)
 
 
 # ### Load Cell Health Profile Labels
-# 
+#
 
 # In[3]:
 
@@ -56,7 +56,7 @@ cell_health_labels
 
 
 # ### Derive classification profile and cell health label correlations (multi-class models)
-# 
+#
 
 # In[4]:
 
@@ -65,37 +65,41 @@ print("Deriving multi-class model correlations...")
 # list for compiling tidy long correlation data
 compiled_tidy_long_corrs = []
 for classification_profiles_path in MCM_classification_profiles_save_dir.iterdir():
-    
+
     # get information about the current model
     model_type = classification_profiles_path.name.split("__")[0]
     feature_type = classification_profiles_path.name.split("__")[1]
-    
-    print(f"Deriving correlations for {model_type} model with {feature_type} features...")
-    
+
+    print(
+        f"Deriving correlations for {model_type} model with {feature_type} features..."
+    )
+
     # load classification profiles
     classification_profiles = pd.read_csv(classification_profiles_path, sep="\t")
-    
+
     # combine cell health label profiles and classification profiles on perturbation and cell line
     final_profile_dataframe = pd.merge(
         cell_health_labels,
         classification_profiles,
         on=["Metadata_pert_name", "Metadata_cell_line"],
     )
-    
+
     # get tidy long correlations for this model's predictions
     model_tidy_long_corrs = validate_utils.get_tidy_long_corrs(final_profile_dataframe)
-    
+
     # add model metadata
     model_tidy_long_corrs["model_type"] = model_type
     model_tidy_long_corrs["feature_type"] = feature_type
-    
+
     # add correlations to compilation list
     compiled_tidy_long_corrs.append(model_tidy_long_corrs)
-    
+
 
 # compile and save tidy long data
 compiled_tidy_long_corrs = pd.concat(compiled_tidy_long_corrs)
-compiled_tidy_long_corrs.to_csv(f"{tidy_long_corrs_save_dir}/compiled_correlations__MCM.tsv", sep="\t")
+compiled_tidy_long_corrs.to_csv(
+    f"{tidy_long_corrs_save_dir}/compiled_correlations__MCM.tsv", sep="\t"
+)
 
 # preview tidy data
 compiled_tidy_long_corrs
@@ -111,39 +115,44 @@ print("Deriving single-class model correlations...")
 compiled_tidy_long_corrs = []
 for phenotypic_class_path in SCM_classification_profiles_save_dir.iterdir():
     for classification_profiles_path in phenotypic_class_path.iterdir():
-        
+
         # get information about the current model
         phenotypic_class = phenotypic_class_path.name
         model_type = classification_profiles_path.name.split("__")[0]
         feature_type = classification_profiles_path.name.split("__")[1]
-        
-        print(f"Deriving correlations for {model_type}, {phenotypic_class} model with {feature_type} features...")
-        
+
+        print(
+            f"Deriving correlations for {model_type}, {phenotypic_class} model with {feature_type} features..."
+        )
+
         # load classification profiles
         classification_profiles = pd.read_csv(classification_profiles_path, sep="\t")
-        
+
         # combine cell health label profiles and classification profiles on perturbation and cell line
         final_profile_dataframe = pd.merge(
             cell_health_labels,
             classification_profiles,
             on=["Metadata_pert_name", "Metadata_cell_line"],
         )
-        
+
         # get tidy long correlations for this model's predictions
-        model_tidy_long_corrs = validate_utils.get_tidy_long_corrs(final_profile_dataframe)
-        
+        model_tidy_long_corrs = validate_utils.get_tidy_long_corrs(
+            final_profile_dataframe
+        )
+
         # add model metadata
         model_tidy_long_corrs["model_type"] = model_type
         model_tidy_long_corrs["feature_type"] = feature_type
-        
+
         # add correlations to compilation list
         compiled_tidy_long_corrs.append(model_tidy_long_corrs)
-        
-    
+
+
 # compile and save tidy long data
 compiled_tidy_long_corrs = pd.concat(compiled_tidy_long_corrs)
-compiled_tidy_long_corrs.to_csv(f"{tidy_long_corrs_save_dir}/compiled_correlations__SCM.tsv", sep="\t")
+compiled_tidy_long_corrs.to_csv(
+    f"{tidy_long_corrs_save_dir}/compiled_correlations__SCM.tsv", sep="\t"
+)
 
 # preview tidy data
 compiled_tidy_long_corrs
-
--- utils/validate_utils.py
+++ utils/validate_utils.py
@@ -10,95 +10,34 @@ from ccc.coef import ccc
 from scipy.spatial.distance import squareform
 
 
-def create_classification_profiles(
-    plate_classifications_dir: pathlib.Path, cell_line_plates: dict
+def get_cell_health_corrs(
+    final_profile_dataframe: pd.DataFrame, corr_type: str, cell_line: str
 ) -> pd.DataFrame:
     """
-    create classsification profiles for correlation to cell health label profiles
-    a classification profile consists of classification probabilities averaged across perturbation and cell line
+    get correlations between cell health indicators and classification profiles
+    correlations are output in wide format
 
     Parameters
     ----------
-    plate_classifications_dir : pathlib.Path
-        path to plate classifications directory
-    cell_line_plates : dict
-        cell line names, each with a list of plate names that are correlated to their respective cell line
+    final_profile_dataframe : pd.DataFrame
+        dataframe with cell health indicator and classification profiles matched by perturbation
+    corr_type : str
+        type of correlation ("pearson" or "ccc")
+    cell_line : str
+        cell line metadata to add to correlations
 
     Returns
     -------
     pd.DataFrame
-        classification profiles dataframe
+        wide format dataframe with correlation info
     """
 
-    cell_line_classification_profiles = []
-
-    for cell_line in cell_line_plates:
-        # create one large dataframe for all 3 plates in the particular cell line
-        cell_line_plate_names = cell_line_plates[cell_line]
-        cell_line_plate_classifications = []
-        for cell_line_plate_name in cell_line_plate_names:
-            plate_classifications = pathlib.Path(
-                f"{plate_classifications_dir}/{cell_line_plate_name}__cell_classifications.csv.gz"
-            )
-            plate_classifications = pd.read_csv(
-                plate_classifications, compression="gzip", index_col=0
-            )
-            cell_line_plate_classifications.append(plate_classifications)
-
-        cell_line_plate_classifications = pd.concat(
-            cell_line_plate_classifications, axis=0
-        ).reset_index(drop=True)
-
-        # create dataframe with cell classifications averaged across perturbation, include cell line metadata
-        # add cell line metadata
-        cell_line_plate_classifications["Metadata_cell_line"] = cell_line
-        # rename perturbation column to match the format of cell health label profiles, in this case "perturbation" corresponds to "reagent" because DeepProfiler (used much earlier in pipeline) makes no distinction
-        cell_line_plate_classifications = cell_line_plate_classifications.rename(
-            columns={"Metadata_Reagent": "Metadata_pert_name"}
-        )
-
-        # get rid of extra metadata columns
-        phenotypic_classes = [
-            col
-            for col in cell_line_plate_classifications.columns.tolist()
-            if ("Metadata" not in col)
-            and (col not in ["Location_Center_X", "Location_Center_Y"])
-        ]
-        columns_to_keep = [
-            "Metadata_pert_name",
-            "Metadata_cell_line",
-        ] + phenotypic_classes
-        cell_line_plate_classifications = cell_line_plate_classifications[
-            columns_to_keep
-        ]
-
-        # average across perturbation
-        cell_line_classification_profile = cell_line_plate_classifications.groupby(
-            ["Metadata_pert_name", "Metadata_cell_line"]
-        ).mean()
-        cell_line_classification_profiles.append(cell_line_classification_profile)
-
-    classification_profiles = pd.concat(cell_line_classification_profiles, axis=0)
-    # convert pandas index to columns
-    classification_profiles = classification_profiles.reset_index(
-        level=["Metadata_pert_name", "Metadata_cell_line"]
-    )
-    # convert no reagent to empty in pert column to follow format of cell health label profiles
-    classification_profiles = classification_profiles.replace(
-        {"Metadata_pert_name": "no reagent"}, "EMPTY"
-    )
-
-    return classification_profiles
-
-
-def get_cell_health_corrs(
-    final_profile_dataframe: pd.DataFrame, corr_type: str, cell_line: str
-):
     # remove metadata columns
     cleaned_final_profile_dataframe = final_profile_dataframe.drop(
         columns=["Metadata_profile_id", "Metadata_pert_name", "Metadata_cell_line"]
     )
 
+    # get correlations depending on type
     if corr_type == "pearson":
         corrs = cleaned_final_profile_dataframe.corr(method="pearson")
     if corr_type == "ccc":
@@ -123,7 +62,23 @@ def get_cell_health_corrs(
     return corrs
 
 
-def get_tidy_long_corrs(final_profile_dataframe: pd.DataFrame):
+def get_tidy_long_corrs(final_profile_dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    get correlation values cell health indicators and classification profiles in tidy long format
+    correlations are derived for each cell line and all cell lines
+    pearson and ccc correlations are derived
+
+    Parameters
+    ----------
+    final_profile_dataframe : pd.DataFrame
+        dataframe with cell health indicator and classification profiles matched by perturbation
+
+    Returns
+    -------
+    pd.DataFrame
+        correlations in tidy long format
+    """
+
     compiled_tidy_long_corrs = []
 
     # get correlations for all cell lines
@@ -140,12 +95,14 @@ def get_tidy_long_corrs(final_profile_dataframe: pd.DataFrame):
         cell_line_pearson_corrs = get_cell_health_corrs(
             cell_line_profiles, "pearson", cell_line
         )
-        cell_line_ccc_corrs = get_cell_health_corrs(cell_line_profiles, "ccc", cell_line)
+        cell_line_ccc_corrs = get_cell_health_corrs(
+            cell_line_profiles, "ccc", cell_line
+        )
         compiled_tidy_long_corrs += [cell_line_pearson_corrs, cell_line_ccc_corrs]
 
     # combine all correlations
     compiled_tidy_long_corrs = pd.concat(compiled_tidy_long_corrs)
-    
+
     # convert wide dataframe to tidy long format
     compiled_tidy_long_corrs = pd.melt(
         compiled_tidy_long_corrs,
