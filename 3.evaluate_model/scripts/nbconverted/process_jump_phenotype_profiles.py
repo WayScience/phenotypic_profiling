3a7957748963001994c63145fb5909844e15df1d
gwaybio <gregory.way@gmail.com>
2024-02-19 13:20:18 -0700

    add umap

--- 3.evaluate_model/scripts/nbconverted/process_jump_phenotype_profiles.py
+++ 3.evaluate_model/scripts/nbconverted/process_jump_phenotype_profiles.py
@@ -16,12 +16,16 @@
 # 3. Convert it to wide format
 # 
 # This wide format represents a "phenotypic profile" which we can use similarly as an image-based morphology profile.
+# 
+# We also fit a UMAP to this phenotypic profile for downstream visualization.
 
 # In[1]:
 
 
 import pathlib
 import pandas as pd
+
+import umap
 
 
 # In[2]:
@@ -57,6 +61,8 @@ output_dir = "jump_phenotype_profiles"
 top_results_summary_file = pathlib.Path(output_dir, "jump_most_significant_phenotype_enrichment.tsv")
 final_jump_phenotype_file = pathlib.Path(output_dir, "jump_phenotype_profiles.tsv.gz")
 shuffled_jump_phenotype_file = pathlib.Path(output_dir, "jump_phenotype_profiles_shuffled.tsv.gz")
+
+jump_umap_file = pathlib.Path(output_dir, "jump_phenotype_profiling_umap.tsv.gz")
 
 
 # ## Load and process data
@@ -172,19 +178,25 @@ jump_pred_df.query("Metadata_model_type == 'final'").phenotype.value_counts()
 # In[12]:
 
 
+metadata_columns = [
+    "Metadata_Plate",
+    "treatment",
+    "treatment_type",
+    "Cell_type",
+    "Time",
+    "Metadata_Well",
+    "cell_count"
+]
+
+
+# In[13]:
+
+
 jump_wide_final_df = (
     jump_pred_df
     .query("Metadata_model_type == 'final'")
     .drop(columns=["p_value"])
-    .pivot(index=[
-        "Metadata_Plate",
-        "treatment",
-        "treatment_type",
-        "Cell_type",
-        "Time",
-        "Metadata_Well",
-        "cell_count"
-    ], columns="phenotype", values="comparison_metric_value")
+    .pivot(index=metadata_columns, columns="phenotype", values="comparison_metric_value")
     .reset_index()
 )
 
@@ -194,22 +206,14 @@ print(jump_wide_final_df.shape)
 jump_wide_final_df.head()
 
 
-# In[13]:
+# In[14]:
 
 
 jump_wide_shuffled_df = (
     jump_pred_df
     .query("Metadata_model_type == 'shuffled'")
     .drop(columns=["p_value"])
-    .pivot(index=[
-        "Metadata_Plate",
-        "treatment",
-        "treatment_type",
-        "Cell_type",
-        "Time",
-        "Metadata_Well",
-        "cell_count"
-    ], columns="phenotype", values="comparison_metric_value")
+    .pivot(index=metadata_columns, columns="phenotype", values="comparison_metric_value")
     .reset_index()
 )
 
@@ -217,4 +221,63 @@ jump_wide_shuffled_df.to_csv(shuffled_jump_phenotype_file, sep="\t", index=False
 
 print(jump_wide_shuffled_df.shape)
 jump_wide_shuffled_df.head()
+
+
+# ## Apply UMAP to phenotypic profiles
+
+# In[15]:
+
+
+umap_random_seed = 123
+umap_n_components = 2
+
+feature_columns = jump_wide_final_df.drop(columns=metadata_columns).columns.tolist()
+print(len(feature_columns))
+
+
+# In[16]:
+
+
+# Initialize UMAP
+umap_fit = umap.UMAP(random_state=umap_random_seed, n_components=umap_n_components)
+
+# Fit UMAP and convert to pandas DataFrame
+embeddings = pd.DataFrame(
+    umap_fit.fit_transform(jump_wide_final_df.loc[:, feature_columns]),
+    columns=[f"UMAP{x}" for x in range(0, umap_n_components)],
+)
+
+# Combine with metadata
+umap_with_metadata_df = pd.concat([jump_wide_final_df.loc[:, metadata_columns], embeddings], axis=1).assign(model_type="final")
+
+
+# In[17]:
+
+
+# Initialize UMAP
+umap_fit = umap.UMAP(random_state=umap_random_seed, n_components=umap_n_components)
+
+# Fit UMAP and convert to pandas DataFrame
+embeddings = pd.DataFrame(
+    umap_fit.fit_transform(jump_wide_shuffled_df.loc[:, feature_columns]),
+    columns=[f"UMAP{x}" for x in range(0, umap_n_components)],
+)
+
+# Combine with metadata
+umap_shuffled_with_metadata_df = pd.concat([jump_wide_shuffled_df.loc[:, metadata_columns], embeddings], axis=1).assign(model_type="shuffled")
+
+
+# In[18]:
+
+
+# Output file
+umap_full_df = pd.concat([
+    umap_with_metadata_df,
+    umap_shuffled_with_metadata_df
+], axis="rows")
+
+umap_full_df.to_csv(jump_umap_file, sep="\t", index=False)
+
+print(umap_full_df.shape)
+umap_full_df.head()
 
